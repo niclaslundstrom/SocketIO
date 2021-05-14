2dd93ac3e906dcc4e260ec59891bfd4f373f2d36
NiclasLundstrom <lille_niclas@hotmail.com>
2021-05-14 15:45:15 +0200

    added axios

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
-const response = await axios.get('https://opentdb.com/api.php?amount=10&category=11&difficulty=medium&type=multiple')
+const axios = require("axios")
 
+
+const req = async () => {
+  const response = await axios.get('https://opentdb.com/api.php?amount=1&category=11&difficulty=medium&type=multiple')
+  console.log(response.data.results)
+}
+req()
+
+
+const path = require('path')
 const express = require('express')
 const app = express()
 const http = require('http')
+
 const httpServer = http.createServer(app)
 const io = require('socket.io')(httpServer)
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-})
+app.use(express.static(path.join(__dirname,'/Public')))
+
 
 httpServer.listen(3000, () => {
     console.log('Server is listening on port 3000 ...');
 });
-
 
 
 
