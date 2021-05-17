3b50d74cc06d8b84cead2183c89f8e7eb8b8aa6e
NiclasLundstrom <lille_niclas@hotmail.com>
2021-05-17 15:01:35 +0200

    rando the array

--- Public/client.js
+++ Public/client.js
@@ -1,1 +1,25 @@
-const socket = io()
+const socket = io('/');
+
+const q = document.getElementById('q')
+const log = document.getElementById('log');
+const roomSelector = document.getElementById('roomSelector');
+const input = document.querySelector("input[type='text']");
+
+console.log('hej')
+
+
+socket.on('question', msg => {
+    console.log('hej2')
+    q.innerHTML  = msg;
+})
+/*
+socket.on("message", message => {
+    const span = document.createElement("span");  
+    span.style.display = "block";  
+    span.textContent = message;
+    log.appendChild(span);
+  });
+
+function changeRoom() {
+    socket.emit('joinRoom', roomSelector.value);
+}*/
--- index.js
+++ index.js
@@ -1,37 +1,61 @@
 const axios = require("axios")
 
 
-const req = async () => {
-  const response = await axios.get('https://opentdb.com/api.php?amount=1&category=11&difficulty=medium&type=multiple')
-  console.log(response.data.results)
-}
-req()
-
 
 const path = require('path')
 const express = require('express')
 const app = express()
 const http = require('http')
-
 const httpServer = http.createServer(app)
 const io = require('socket.io')(httpServer)
+
+const req = async () => {
+  console.log('inne i funktion req')
+    const response = await axios.get('https://opentdb.com/api.php?amount=1&category=11&difficulty=medium&type=multiple')
+    //console.log(response.data.results[0])
+    let data = response.data.results[0]
+    question(data)
+  }
+
+
+function question(data)  {
+    //console.log(data.question);
+    let array = [];
+    array.push(data.correct_answer)
+    array.push(data.incorrect_answers[0]);
+    array.push(data.incorrect_answers[1]);
+    array.push(data.incorrect_answers[2]);
+     for (let i = array.length -1; i > 0; i--) {
+       let j = Math.floor(Math.random() * (i + 1));
+       var temp = array[i];
+       array[i] = array[j];
+       array[j] = temp;
+     }
+    let question = data.question
+    console.log(array)
+    return question, array
+
+}
+
+
 app.use(express.static(path.join(__dirname,'/Public')))
+
+io.of("/").on("connect", (socket) => {
+ 
+    req();
+    console.log(`client with id ${socket.id} connected`);
+  socket.on('question', msg =>{
+    io.of('/').emit('question', `${msg.array}`)
+  })
+    socket.send(`Hello Player ${socket.id}, lets start the game`);
+    socket.on("disconnect", () => {
+      console.log(`client with id ${socket.id} disconnected`);
+    });
+ 
+});
 
 
 httpServer.listen(3000, () => {
     console.log('Server is listening on port 3000 ...');
 });
 
-
-
-
-
-/* Randomize array in-place using Durstenfeld shuffle algorithm 
-function shuffleArray(array) {
-    for (var i = array.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }>
-}*/
