1e729bda761fa039ba54541cbdc0c1b6c4c0ab49
Pob74 <70943669+Pob74@users.noreply.github.com>
2021-05-18 17:39:53 +0200

    removed &#039 and similar from code

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const axios = require("axios")
+const axios = require('axios')
 const path = require('path')
 const express = require('express')
 const app = express()
@@ -6,23 +6,26 @@ const http = require('http')
 const httpServer = http.createServer(app)
 const io = require('socket.io')(httpServer)
 
-
 let correctAnswer = null
 function question(data) {
   // console.log(data.question);
-  let array = [];
+  let array = []
   correctAnswer = data.correct_answer
   array.push(data.correct_answer)
-  array.push(data.incorrect_answers[0]);
-  array.push(data.incorrect_answers[1]);
-  array.push(data.incorrect_answers[2]);
+  array.push(data.incorrect_answers[0])
+  array.push(data.incorrect_answers[1])
+  array.push(data.incorrect_answers[2])
   for (let i = array.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * (i + 1));
-    var temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
+    let j = Math.floor(Math.random() * (i + 1))
+    var temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
   }
   let question = data.question
+    .replace(/&quot;/g, '"')
+    .replace(/&#039;/g, "'")
+    .replace(/&ldquo;/g, "'")
+    .replace(/&amp;/g, '&')
   return { question, array }
 }
 
@@ -32,14 +35,16 @@ let player = null
 let activeRoom = 'playerRoom'
 let data = null
 const sendQuestion = async (socket) => {
-  const response = await axios.get('https://opentdb.com/api.php?amount=1&category=11&difficulty=medium&type=multiple')
+  const response = await axios.get(
+    'https://opentdb.com/api.php?amount=1&category=11&difficulty=medium&type=multiple'
+  )
   // console.log(response.data.results[0])
   data = response.data.results[0]
   socket.emit('question', question(data))
   io.in('SpectatorRoom').emit('spectatorQ', question(data))
 }
 
-io.of("/").on("connect", (socket) => {
+io.of('/').on('connect', (socket) => {
   if (player === null) {
     player = socket.id
     console.log(activeRoom)
@@ -55,9 +60,9 @@ io.of("/").on("connect", (socket) => {
     console.log('spectator')
     socket.emit('spectatorQ', question(data))
   }
-  console.log(`client with id ${socket.id} connected`);
+  console.log(`client with id ${socket.id} connected`)
 
-  socket.on('checkAnswer', answer => {
+  socket.on('checkAnswer', (answer) => {
     if (correctAnswer === answer) {
       console.log('correct')
       sendQuestion(socket)
@@ -67,24 +72,24 @@ io.of("/").on("connect", (socket) => {
       console.log('ERRORRRR')
       sendQuestion(socket)
       // io.in('SpectatorRoom').emit('spectatorQ', question(data))
-      io.in('SpectatorRoom').emit('spectatorA', { answer, correct: 'Incorrect' })
-
+      io.in('SpectatorRoom').emit('spectatorA', {
+        answer,
+        correct: 'Incorrect'
+      })
     }
   })
   /* socket.emit('question', msg => {
       io.of('/').emit('question', `${msg.array}`)
     })
     socket.send(`Hello Player ${socket.id}, lets start the game`);*/
-  socket.on("disconnect", () => {
+  socket.on('disconnect', () => {
     if (socket.id === player) {
       player = null
     }
-    console.log(`client with id ${socket.id} disconnected`);
-  });
-
-});
+    console.log(`client with id ${socket.id} disconnected`)
+  })
+})
 
 httpServer.listen(3000, () => {
-  console.log('Server is listening on port 3000 ...');
-});
-
+  console.log('Server is listening on port 3000 ...')
+})
